e1e91d4f8be1325a06a08417e194b2f421a4e1b8
p-a <peter.augustinak@gmail.com>
2022-07-30 15:55:45 +0200

    Step 4: Adding and editing cards from UI, navigation

--- cards/views.py
+++ cards/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
+    CreateView,
+    UpdateView,
 )
 
 from .models import Card
 
+# Create your views here.
+
+
 class CardListView(ListView):
     model = Card
     queryset = Card.objects.all().order_by('box', '-date_created')
+
+
+class CardCreateView(CreateView):
+    model = Card
+    fields = ["question", "answer", "box"]
+    success_url = reverse_lazy("card-create")
+
+
+class CardUpdateView(CardCreateView, UpdateView):
+    model = Card
+    success_url = reverse_lazy("card-list")
